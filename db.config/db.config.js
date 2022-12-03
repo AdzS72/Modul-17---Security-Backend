27575f0e3b7b07dedcd03f020fcac8dc270f5091
Ayumas Qonita <80805626+AdzS72@users.noreply.github.com>
2022-12-03 13:28:57 +0700

    Add files via upload

--- db.config/db.config.js
+++ db.config/db.config.js
@@ -1,13 +1,12 @@
-const { Client } = require("pg");
-require("dotenv").config();
+const { Client } = require('pg');
+require('dotenv').config()
 
 const db = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  url: process.env.DB_URL,
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.db_port,
 });
 
 module.exports = db;
